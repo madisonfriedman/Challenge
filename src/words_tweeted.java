8be59abb3ca0f7c0257e2eea22de29a925e316e0
Madison John Friedman <mjf2174@columbia.edu>
2015-07-11 23:04:36 -0700

    added batch import ability

--- src/words_tweeted.java
+++ src/words_tweeted.java
@@ -8,7 +8,6 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
-import java.io.IOException;
 
 public class words_tweeted {
 	
@@ -29,88 +28,109 @@ public class words_tweeted {
 		int capacity = 5000000;
 		ConcurrentHashMap<String, Counter> hm = new ConcurrentHashMap<String, Counter>(capacity);
 
+		/*NOTE: using BufferedInputStream to read in file all at once (or large chunks at a time) is much faster than
+		 * BufferedReader. Separating reading and parsing actions saves a lot of time in I/O heavy tasks like this one.
+		 */
+
 		//read input from tweets.txt
 		String tweetFile = runDirectory + "/tweet_input/tweets.txt";
 		File file = new File(tweetFile);
 		FileInputStream fi = new FileInputStream(file);
-		BufferedInputStream bi = new BufferedInputStream(fi);
-
-		/*NOTE: using BufferedInputStream to read in file all at once is much faster than BufferedReader.
-		 * Separating reading and parsing actions saves a lot of time in I/O heavy tasks like this one.
-		 */
+		BufferedInputStream bis = new BufferedInputStream(fi);
 		
-		//read entire file into single byte array
-		int fileSize = (int) file.length();
-		byte[] bytes = new byte[fileSize];
-		bi.read(bytes, 0, fileSize);
-		
-		bi.close();
-		fi.close();
-		
-		//convert byte[] to String, UTF-8 is backwards compatable with ASCII
-		String fileString = new String(bytes, "UTF-8");
-		bytes = null;
-		
-		//Thread array of size N + 1 where N is the number of computer processors
-		int threadNum = 5;
-		Consumer[] c = new Consumer[threadNum];
-		Thread[] consumers = new Thread[threadNum];
-		
-		//subString indecies
-		int start = 0;
-		int end = 0;
-		String subString;
-		
-		//split fileString into substrings for parallel processing
-		for(int i = 0; i < threadNum; i++){
-			
-			end = ( (fileSize * (i + 1)) / threadNum);
-			end = Math.max(end, fileString.indexOf(' ', end));
-			
-			//add newline for parsing purposes
-			subString = fileString.substring(start, end);
-			subString = subString + "\n";
-			
-			c[i] = new Consumer(hm, subString);
-			consumers[i] = new Thread(c[i]);
-			consumers[i].start();
-			
-			start = end + 1;
-		}
-		
-		for(int i = 0; i < consumers.length; i++){
-			consumers[i].join();
-		}
-		
-		/*NOTE: Using HashMap w/ merge sort is preferable to using TreeMap w/o merge sort.
-		 * TreeMap gives O(log(n)) put/get/containsKey time but its really t*O(log(n)) where t is the number
-		 * of tweets; HashMap gives t*O(1) put/get/containsKey time with only one merge sort at O(n*log(n)).
-		 * Because the number of tweets, t, is much larger (5*10^8 tweets per day) than the size of the  
-		 * HashMap, n (there are only 10^6 english words), it would be far slower to use the TreeMap.
-		 * t*O(1) + O(n*log(n)) << t*O(log(n)); therefore HashMap w/ merge sort is faster.
-		 *
-		 * Also note that while merge sort is a prime candidate for multithreading, becuase our HashMap is
-		 * small, resulting performance gains are negligible.
-		 * https://courses.cs.washington.edu/courses/cse373/13wi/lectures/03-13/MergeSort.java
-		 */
-		
-		//sort the HashMap values by ASCII code
-		List<String> sortedKeys = new ArrayList<String>(hm.keySet());
-		Collections.sort(sortedKeys);
-		
-		//output formatted word/count to text file
+		//for outputting formatted word/count to text file
 		String ft1 = runDirectory + "/tweet_output/ft1.txt";
 		File output = new File(ft1);
 		FileWriter fw = new FileWriter(output);
 		BufferedWriter bw = new BufferedWriter(fw);
+
+		//max batch import size in MB, decrease the value if you are getting "OutOfMemoryError" and
+		//increasing JVM max heap size is impossible
+		long userArraySizeLimit = 2000;
 		
-		Iterator<String> it = sortedKeys.iterator();
-		for (int j = 0; j < sortedKeys.size(); j++){
-			String s = it.next();
-			bw.write(String.format("%-30s%d", s, hm.get(s).returnCount()));
-			bw.newLine();
+		//max array length
+		int arrayMaxSize = (int) Math.min(Integer.MAX_VALUE - 5, 1000000*userArraySizeLimit);
+		
+		//text file's size in bytes
+		long fileSize = (int) file.length();
+		
+		//number of individual imports
+		int numberOfBisReads = (int) (fileSize / arrayMaxSize) + 1;
+		
+		long bytesRemaining = fileSize;
+		
+		for(int i = 0; i < numberOfBisReads; i++){
+		
+			//read entire file (or section of file) into single byte array
+			int arraySize = (int) Math.min(arrayMaxSize, bytesRemaining);
+			byte[] bytes = new byte[arraySize];
+			bis.read(bytes, 0, arraySize);
+			
+			//convert byte[] to String, UTF-8 is backwards compatable with ASCII
+			String fileString = new String(bytes, "UTF-8");
+			bytes = null;
+			
+			//Thread array of size N + 1 where N is the number of computer processors
+			int threadNum = 5;
+			Consumer[] c = new Consumer[threadNum];
+			Thread[] consumers = new Thread[threadNum];
+			
+			//subString indecies
+			int start = 0;
+			int end = 0;
+			String subString;
+			
+			//split fileString into substrings for parallel processing
+			for(int j = 0; j < threadNum; j++){
+				
+				end = ( ((int)arraySize * (j + 1)) / threadNum);
+				end = Math.max(end, fileString.indexOf(' ', end));
+				
+				//add newline for parsing purposes
+				subString = fileString.substring(start, end);
+				subString = subString + "\n";
+				
+				c[j] = new Consumer(hm, subString);
+				consumers[j] = new Thread(c[j]);
+				consumers[j].start();
+				
+				start = end + 1;
+			}
+			
+			for(int j = 0; j < consumers.length; j++){
+				consumers[j].join();
+			}
+			
+			/*NOTE: Using HashMap w/ merge sort is preferable to using TreeMap w/o merge sort.
+			 * TreeMap gives O(log(n)) put/get/containsKey time but its really t*O(log(n)) where t is the number
+			 * of tweets; HashMap gives t*O(1) put/get/containsKey time with only one merge sort at O(n*log(n)).
+			 * Because the number of tweets, t, is much larger (5*10^8 tweets per day) than the size of the  
+			 * HashMap, n (there are only 10^6 english words), it would be far slower to use the TreeMap.
+			 * t*O(1) + O(n*log(n)) << t*O(log(n)); therefore HashMap w/ merge sort is faster.
+			 *
+			 * Also note that while merge sort is a prime candidate for multithreading, becuase our HashMap is
+			 * small, resulting performance gains are negligible.
+			 * https://courses.cs.washington.edu/courses/cse373/13wi/lectures/03-13/MergeSort.java
+			 */
+			
+			//sort the HashMap values by ASCII code
+			List<String> sortedKeys = new ArrayList<String>(hm.keySet());
+			Collections.sort(sortedKeys);
+			
+			//format and print words and counts
+			Iterator<String> it = sortedKeys.iterator();
+			for (int j = 0; j < sortedKeys.size(); j++){
+				String s = it.next();
+				bw.write(String.format("%-30s%d", s, hm.get(s).returnCount()));
+				bw.newLine();
+			}
+
+			bytesRemaining = bytesRemaining - arraySize;
+			
 		}
 		
+		bis.close();
+		fi.close();
 		bw.close();
 		
 		System.out.println("execution time in ms: " + (System.currentTimeMillis() - time));
@@ -193,22 +213,5 @@ public class words_tweeted {
 			return this.count;
 		}
 	}
-	
-	//sample skip function from stackoverflow to help read in files > 2GB
-	public static void skip(BufferedInputStream bis, long n) throws IOException {
-    		while(n > 0) {
-        		long n1 = bis.skip(n);
-	 		if( n1 > 0 ) {
-            			n -= n1;
-        		} 
-        		else if( n1 == 0 ) {
-            			if( bis.read() == -1)  // EOF
-                			break;
-            			else 
-                			n--;
-        		} 
-        		else 
-        			throw new IOException("skip() returned a negative value");
-    		}
-	}
+
 }
